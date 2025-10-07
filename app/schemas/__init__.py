1c8d5285ec27d656c0923324809cba67ca6ebe4c
njcross <njcross1990@gmail.com>
2025-10-07 05:31:37 -0500

    Update __init__.py

--- app/schemas/__init__.py
+++ app/schemas/__init__.py
@@ -1,4 +1,34 @@
 # app/schemas/__init__.py
-from .auth import EmailRequest, UserOut  # add any other Pydantic models you expose
+from .auth import (
+    EmailRequest,
+    MagicLinkResponse,
+    ErrorResponse,
+    MeUser,
+    MeResponse,
+    LogoutResponse,
+    CallbackOK,
+)
+from .health import (
+    HealthResponse, 
+    RedisPingResponse,
+    DBVersionResponse,
+    ErrorResponse
+)
+from .protected import (
+    WhoAmIResponse
+)
 
-__all__ = ["EmailRequest", "UserOut"]
+__all__ = [
+    "EmailRequest",
+    "MagicLinkResponse",
+    "ErrorResponse",
+    "MeUser",
+    "MeResponse",
+    "LogoutResponse",
+    "CallbackOK",
+    "HealthResponse",
+    "RedisPingResponse",
+    "DBVersionResponse",
+    "ErrorResponse",
+    "WhoAmIResponse",
+]
